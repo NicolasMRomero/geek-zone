d903cda22fc99cff17f73d235cfa8c8d8d3e9d99
NicolasMRomero <nicolas.m.romero@gmail.com>
2018-06-04 20:14:23 -0300

    Vista index, estilos, imagenes y videos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,170 @@
+@extends('layout')
+
+@section('content')
+  <section class="slider">
+      <div id="sliderPortada" class="carousel slide" data-ride="carousel">
+         <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img class="d-block w-100" src="images/slider/slider_star_wars.jpg" alt="imagen star wars">
+             <div class="carousel-caption">
+               <h5>Star Wars</h5>
+               <p class="d-none d-md-block">Recuerda, la Fuerza estará contigo… siempre</p>
+             </div>
+           </div>
+           <div class="carousel-item">
+             <img class="d-block w-100" src="images/slider/slider_the_simpsons.jpg" alt="Second slide">
+             <div class="carousel-caption d-none d-md-block">
+               <h5>The Simpsons</h5>
+               <p class="d-none d-md-block"> "Jeff" Albertson estaría orgulloso de nosotros</p>
+             </div>
+           </div>
+           <div class="carousel-item">
+             <img class="d-block w-100" src="images/slider/slider_transformers.jpg" alt="Third slide">
+             <div class="carousel-caption d-none d-md-block">
+               <h5>Transformers</h5>
+               <p class="d-none d-md-block">Llegados directamente desde Cybertron!</p>
+             </div>
+           </div>
+         </div>
+         <a class="carousel-control-prev" href="#sliderPortada" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Anterior</span>
+         </a>
+         <a class="carousel-control-next" href="#sliderPortada" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Siguiente</span>
+         </a>
+       </div>
+    </section>
+    <section class="encabezado" >
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="text-center productos">PRODUCTOS DESTACADOS</h1>
+          </div>
+        </div>
+        <div class="row productos">
+          <div class="col-12 col-md-4 col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="images/productos/kylo.jpg" alt="Kylo Ren">
+              <div class="card-body">
+                <h5 class="card-title">Kylo Ren</h5>
+                <p class="card-text">Desde Star Wars Episodio VII - The Force Awakens, ¡llega Kylo Ren Pop!</p>
+                <a href="#" class="btn d-block agregar-al-carrito">Agregar al carrito  <i class="fas fa-shopping-cart"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="images/productos/optimus-prime.jpg" alt="Optimus Prime">
+              <div class="card-body">
+                <h5 class="card-title">Optimus Prime</h5>
+                <p class="card-text">Derrotá a los decepticons junto líder de los Autobots. Al alcance de tu mano!</p>
+                <a href="#" class="btn d-block agregar-al-carrito">Agregar al carrito  <i class="fas fa-shopping-cart"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="images/productos/krusty.jpg" alt="Payaso Krusty">
+              <div class="card-body">
+                <h5 class="card-title">Payaso Krusty</h5>
+                <p class="card-text">Si! Es Krusty, el payaso de la tele! Un producto de la "Korporación Krusty"</p>
+                <a href="#" class="btn d-block agregar-al-carrito">Agregar al carrito  <i class="fas fa-shopping-cart"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<!--Productos por Categoria-->
+<section class="encabezado" >
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center pb-4">CATEGORÍAS PRINCIPALES</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <img class="card-img-top" src="images/simpsons/simpsons-categoria2.jpg" alt="imagen the simpsons">
+          <div class="card-body">
+            <h2 class="card-text text-center">THE SIMPSONS</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <img class="card-img-top" src="images/star-wars/lego-starwars.jpg" alt="imagen star wars">
+          <div class="card-body">
+            <h2 class="card-text text-center">STAR WARS</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <img class="card-img-top" src="images/transformers/transformers.jpg" alt="imagen transformers">
+          <div class="card-body">
+            <h2 class="card-text text-center">TRANSFORMERS</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <img class="card-img-top" src="images/funko/funko-harry.jpg" alt="imagen funko">
+          <div class="card-body">
+            <h2 class="card-text text-center">FUNKO</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <img class="card-img-top" src="images/tienda-geek/space-invaders.jpg" alt="imagen tienda geek">
+          <div class="card-body">
+            <h2 class="card-text text-center">TIENDA GEEK</h2>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="card mb-3" style="width: 18rem;">
+          <video class="card-img-top" src="videos/videosemana.mp4" type="video.mp4" muted autoplay controls></video>
+          <div class="card-body">
+            <h2 class="card-text text-center">VIDEO DE LA SEMANA</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+</section>
+<!--newsletter-->
+<section>
+  <div class="container-newsletter">
+    <div class="row">
+    <div class="col-12 text-center">
+      <form method="post">
+        <div class="form-group">
+         <label for="exampleInputEmail1">
+           <h6 class="font-weight-bold">
+               <strong>Suscribite a nuestro newsletter</strong>
+           </h6></label>
+           <input type="email" class="form-control form-newsletter" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresá tu email" name="email" value="">
+
+            <br>
+          <button type="submit" class="btn btn-primary">ENVIAR</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  </div>
+</section>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Geek Zone</title>
+    <meta name="description" content="Sitio web e-commerce">
+    <meta name="keywords" content="Star wars, Transformers, The Simpsons, Funko, Tienda Geeks">
+    <meta name="author" content="Pau, Gise, Nico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  </head>
+  <body>
+    <!-- Hearder -->
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light " style="z-index:66">
+        <div class="col-lg-5 collapse navbar-collapse" id="navbarSupportedContent" style="text-align:left">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                Productos
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Star Wars</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Transformers</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Los Simpsons</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Funko</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Tienda Geek</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="faq.php">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="perfil.php" style="">Mi Perfil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="ingresar.php" style="">Ingresar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="registrarse.php" style="">Registrarse</a>
+            </li>
+              </ul>
+                </div>
+              <div class="col-lg-2"  style="text-align:center">
+              <a class="navbar-brand logo-menu" href="index.php"><img src="images/logo-E.png" alt="logo-E"></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              </div>
+          <div class="col-lg-5"  style="text-align:right">
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0 buscar" type="submit"><i class="fas fa-search"></i></button>
+            </form>
+            </div>
+      </nav>
+    </header>
+
+    @yield('content')
+
+
+  <!--Footer-->
+  <footer>
+    <div class="container mt-3 mb-3 text-center text-md-left">
+        <div class="row mt-3">
+            <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>GEEK ZONE</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Pensado para los aficionados a la tecnología, los coleccionistas y los fanáticos de la ciencia ficción, Star Wars, Transformers, The Simpsons y los Funkos.</p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>Productos</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a class="link-footer" href="#!">Star Wars</a>
+                </p>
+                <p>
+                    <a class="link-footer"  href="#!">Transformers</a>
+                </p>
+                <p>
+                    <a class="link-footer" href="#!">The Simpsons</a>
+                </p>
+                <p>
+                    <a class="link-footer" href="#!">Funko</a>
+                </p>
+                <p>
+                    <a class="link-footer" href="#!">Tienda Geek</a>
+                </p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>Contacto</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                  <a class="link-footer" href="https://www.google.com.ar/maps/place/Lima+1111,+C1073AAW+CABA/@-34.6210713,-58.383862,17z/data=!3m1!4b1!4m5!3m4!1s0x95bccb28ea8781cb:0x950feb519009506e!8m2!3d-34.6210713!4d-58.3816733" target="_blank">  <i class="fa fa-home mr-3"></i> Calle Falsa 123</a></p>
+                <p>
+                  <a class="link-footer" href="mailto:info@geekzon.com?subjetc=Consula%productos" target="_blank">  <i class="fa fa-envelope mr-3"></i>info@geekzone.com</a></p>
+                <p>
+                  <a class="link-footer" href="tel:5553226" target="_blank">  <i class="fa fa-phone mr-3"></i>5555-3226</a></p>
+            </div>
+          </div>
+          </div>
+    <!-- Copyright-->
+    <div class="container-copyright" style="background-color:white;">
+      <div class="row">
+          <div class="col-12 footer-copyright  pt-2 pb-2 text-center">
+        Diseñado y programado por:
+        <a class="link-footer" href="#">
+            <strong> Pau, Gise y Nico</strong>
+        </a>
+          </div>
+        </div>
+    </div>
+    </footer>
+    <!--/.Copyright -->
+
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+
+    </body>
+  </html>
