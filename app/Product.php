391c175ecaf2a1bb7044830bc7706bc0d21d458a
GiseSosa <aredhel_2@hotmail.com>
2018-06-22 20:47:27 -0300

    con seeder funcionando

--- app/Product.php
+++ app/Product.php
@@ -6,20 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-  protected $table = 'products';
-  protected $primaryKey = 'id';
+
   protected $guarded = [];
 
-<<<<<<< HEAD
-  public function user()
-  {
-      return $this->belongsTo(User::class);
-  }
-=======
-  // Del archivo de Jeff
-  public function user()
-   {
-       return $this->belongsTo(User::class);
-   }
->>>>>>> a1c5e5cb746fbd9c3fb9415de5e0c0acb9885415
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
