970dfca65a77b494bec8bcb536db03eac22cb4d5
NicolasMRomero <nicolas.m.romero@gmail.com>
2018-06-28 22:20:22 -0300

    correccion de las regex

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -138,17 +138,18 @@
   var form = document.forms[1],
       elements = form.elements;
 
-  var emailRegex = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  var emptyRegex = /^(\w+\S+)$/;
+  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
 
   var validateName = function(e){
-    if (form.name.value == 0) {
+    if (!emptyRegex.test(form.name.value)) {
       alert('nombre vacio');
       e.preventDefault();
     }
   };
 
   var validateEmail = function(e){
-    if (form.email.value == 0) {
+    if (!emptyRegex.test(form.email.value)) {
       alert('email vacio');
       e.preventDefault();
     }
