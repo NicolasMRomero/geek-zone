5bd7a8c89d39445e5c5761faf3c1d259848f6d8b
NicolasMRomero <nicolas.m.romero@gmail.com>
2018-06-29 12:03:58 -0300

    correcion de la imagen en vista de producto individual

--- resources/views/show-product.blade.php
+++ resources/views/show-product.blade.php
@@ -5,7 +5,11 @@
 <div class="row">
 		<div class="col-12 col-md-4 col-lg-4">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top img-prod-en-perfil" src="/storage/pdtos/{{ $product->image }}" alt="{{ $product->name }}">
+				@if (!$product->image)
+					<img src="/images/default.png" width="100" alt="">
+				@else
+					<img src="/storage/pdtos/{{ $product->image }}" width="100"  alt="">
+				@endif
 				<div class="card-body">
 					<h5 class="card-title">{{ $product->name }}</h5>
 					<p class="card-text"> $ {{ $product->price }}</p>
